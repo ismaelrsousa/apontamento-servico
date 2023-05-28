8fd49daeb89e3c8188fc4069f3cfc3cb6be0c3b8
Ismael Sousa <ismaelrsousa.contato@gmail.com>
2023-05-28 17:03:37 -0300

    Initial commit

---
+++ ApontamentoServico/Program.cs
@@ -0,0 +1,9 @@
+﻿using Core.Financeiro.ApontamentoServico;
+
+string servico = "CS_IMO1";
+
+ApontamentoServicoBase.Servicos.TryGetValue(servico, out var tipoApontamento);
+
+var apontamento = (ApontamentoServicoBase)Activator.CreateInstance(tipoApontamento ?? typeof(ApontamentoServicoBase));
+
+apontamento.Apontar();
